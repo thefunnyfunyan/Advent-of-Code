02cd505a95a8e8c228b1e85d972055989d5206ea
Brandon <brando15@ksu.edu>
2015-12-07 18:34:00 -0600

    finished part2

---
+++ AdventDay4/Day4Part2.py
@@ -0,0 +1,32 @@
+__author__ = 'Brandon'
+import hashlib
+
+
+def converter(num):
+    totalString = "ckczppom" + str(num)
+    return hashlib.md5(bytes(totalString, 'utf-8')).hexdigest()
+
+def check(hexNum):
+    goodNum = True
+    index = 0
+    while (index < 6 and goodNum == True):
+        if(hexNum[index] != '0'):
+            goodNum = False
+        index += 1
+    return goodNum
+
+
+def main():
+    foundNum = False
+    index = -1
+    while(not foundNum):
+        index += 1
+        foundNum = check(converter(index))
+        if(index%100==0):
+            print("...")
+    print(foundNum)
+    print(index)
+    print(converter(index))
+
+if(__name__ == "__main__"): main()
+
